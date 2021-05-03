645b808e1032bddc650d91e3a7d0ce41f9749617
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-03 23:42:07 +0900

    객체지향 프로그래밍 기초(1)
    1.1 클래스 정의
    1.1.2 클래스의 인스턴스 생성
    1.1.3 객체의 이용
    1.1.4 여러개의 인스턴스 생성
    1.2 구조체

---
+++ Practical_CS_Project/Practical_CS_Project/01_Basic/DT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_CS_Project._01_Basic
+{
+    class DT
+    {
+        class MyClass
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        struct MyStruct
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+
+        static void Main()
+        {
+            DateTime date = new DateTime(2021, 05, 03);
+
+            int year = date.Year;
+
+            DateTime daysAfter10 = date.AddDays(10);
+
+            Console.WriteLine("설정한 날짜 : " + date);
+            Console.WriteLine("설정한 날짜의 년도 : " + year);
+            Console.WriteLine("설정한 날짜의 10일 후 날짜 : " + daysAfter10);
+
+            MyClass myClass = new MyClass { X = 1, Y = 2 };
+
+            MyStruct myStruct = new MyStruct { X = 1, Y = 2 };
+
+        }
+    }
+}
---
+++ Practical_CS_Project/Practical_CS_Project/01_Basic/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_CS_Project._01_Basic
+{
+    public class Product
+    {
+        // 상품 코드
+        public int Code { get; private set; }
+
+        // 상품 이름
+        public string Name { get; private set; }
+
+        // 상품 가격(세금이 붙지 않은 가격)
+        public int Price { get; private set; }
+
+        public Product(int code, string name, int price)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+
+        public int GetPriceIncludingTax()
+        {
+            return Price + GetTax();
+        }
+
+        static void Main()
+        {
+            Product yakkwa = new Product(12, "약과", 180);
+            Product chapssal = new Product(235, "찹쌀떡", 160);
+
+
+            int yakkwaPrice = yakkwa.Price;
+            int yakkwaTaxIncluded = yakkwa.GetPriceIncludingTax();
+
+
+            Console.WriteLine("약과 가격(세금 미포함) : " +  yakkwaPrice);
+            Console.WriteLine("약과 가격(세금 포함) : " + yakkwaTaxIncluded);
+
+            Console.WriteLine();
+            Console.WriteLine(chapssal.Name + " 가격(세금 포함) : " + chapssal.GetPriceIncludingTax()+"\\");
+        }
+    }
+
+}
