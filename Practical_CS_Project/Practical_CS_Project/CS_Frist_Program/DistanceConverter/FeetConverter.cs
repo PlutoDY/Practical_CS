076d2b95d6cea959a3e445cf657b565f4ec9ffd9
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-06 09:32:04 +0900

    Amend Class to Static Class

--- Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/FeetConverter.cs
+++ Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/FeetConverter.cs
@@ -6,12 +6,12 @@ namespace Practical_CS_Project.CS_Frist_Program.DistanceConverter
 {
     public class FeetConverter
     {
-        public double FromMeter(double meter)
+        public static double FromMeter(double meter)
         {
             return meter / 0.3048;
         }
 
-        public double ToMeter(double feet)
+        public static double ToMeter(double feet)
         {
             return feet * 0.3048;
         }
