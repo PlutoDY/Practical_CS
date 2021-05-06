5bfded5e00ad19771b48f633a65bd58f3c377697
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-06 09:47:50 +0900

    Completion FeetConverter

--- Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/FeetConverter.cs
+++ Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/FeetConverter.cs
@@ -4,16 +4,19 @@ using System.Text;
 
 namespace Practical_CS_Project.CS_Frist_Program.DistanceConverter
 {
-    public class FeetConverter
+    public static class FeetConverter
     {
+        //private const double ratio = 0.3048;
+        public static readonly double Ratio = 0.3048;
+
         public static double FromMeter(double meter)
         {
-            return meter / 0.3048;
+            return meter / ratio;
         }
 
         public static double ToMeter(double feet)
         {
-            return feet * 0.3048;
+            return feet * ratio;
         }
     }
 }
