4b7ada092dee11c2e387799138ed8417a6a8237c
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-06 09:01:57 +0900

    Update Program.cs
    Makes computational logic independent in the form of methods

--- Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/Program.cs
+++ Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/DistanceConverter/Program.cs
@@ -6,13 +6,19 @@ namespace Practical_CS_Project.CS_Frist_Program.DistanceConverter
 {
     class Program
     {
-       static void Main(string[] args)
+        static void Main(string[] args)
         {
             for (int feet = 1; feet <= 10; feet++)
             {
-                double meter = feet * 0.3048;
+                double meter = FeetToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
+        }
+        
+        // FeetToMeter 메서드의 시그니처를 결정
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
         }
     }
 }
