145458812175c9dba7f511965ec26e4296ae4cf0
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-11 20:33:43 +0900

    sub1

--- Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/Calculating Revenue/Sale.cs
+++ Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/Calculating Revenue/Sale.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
@@ -11,5 +12,24 @@ namespace Practical_CS_Project.CS_Frist_Program.Calculating_Revenue
         public string ProductCategory { get; set; }
 
         public int Amount { get; set; }
+
+        static List<Sale> ReadSales(string filePath)
+        {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach(string line in lines)
+            {
+                string[] item = line.Split(',');
+                Sale sale = new Sale
+                {
+                    ShopName = item[0],
+                    ProductCategory = item[1],
+                    Amount = int.Parse(item[2])
+                };
+                sales.Add(sale);
+            }
+            
+            return sales;
+        }
     }
 }
