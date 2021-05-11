b39b5b50e8472653d28f7719000e32ed977f0e7c
PlutoDY <43290613+PlutoDY@users.noreply.github.com>
2021-05-11 18:05:22 +0900

    csvFileAdd

---
+++ Practical_CS_Project/Practical_CS_Project/CS_Frist_Program/Calculating Revenue/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practical_CS_Project.CS_Frist_Program.Calculating_Revenue
+{
+    public class Sale
+    {
+        public string ShopName { get; set; }
+        
+        public string ProductCategory { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
